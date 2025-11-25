895c66e20aec01a9b411d4dde0a690714a474efd
kekeiramune <kcinagas123@gmail.com>
2025-11-25 19:47:00 +0700

    new progress maybe

---
+++ urisphynx_ticket/resources/views/components/landing/maincontainer.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full flex justify-center mt-0">
+    <div class="max-w-[1900px] w-full h-[600px] p-10 rounded-[70px] bg-secondary shadow-lg">
+        {{ $slot }}
+    </div>
+</div>
---
+++ urisphynx_ticket/resources/views/components/landing/steps.blade.php
@@ -0,0 +1,7 @@
+<div class="">
+    <span class="landing-page-text18 HeadingsH1">
+            <span>Transaksi aman,</span>
+            <br/>
+            <span>nonton konser nyaman</span>
+          </span>
+</div>
