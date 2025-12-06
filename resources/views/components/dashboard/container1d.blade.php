47b4a28b3630d7d00702ddbde276f81bd32be4c8
kekeiramune <kcinagas123@gmail.com>
2025-12-06 19:18:17 +0700

    dashboard setengah blm done

---
+++ resources/views/components/dashboard/container1d.blade.php
@@ -0,0 +1,11 @@
+<div class="flex gap-6 mt-10 justify-center">
+    <div class="relative top-[310px] left-[80px] w-[300px] h-[300px] bg-white rounded-[24px] flex flex-col items-center justify-center">
+        <img class="w-15 h-15 mx-auto mb-3" src="{{ asset('ticket.svg') }}" alt="">
+
+        <h1 class="font-semibold text-xl text-center" >Active Tickets Available</h1>
+    </div>
+    <div class="flex flex-col relative top-[310px] left-[80px] w-[300px] h-[300px] bg-white rounded-[24px] items-center justify-center">
+        <img class="w-15 h-15 mx-auto mb-3" src="{{ asset('purchase.svg') }}" alt="">
+        <h1 class="font-semibold text-xl text-center">Purchase History</h1>
+    </div>
+</div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,6 +16,6 @@
     <div class="flex items-center gap-5 text-xl">
         <span><img class="w-10 h-10" src="{{ asset('notif.svg') }}" alt=""></span>
         <span><img class="w-10 h-10" src="{{ asset('profile.svg') }}" alt=""></span>
-        <a href="#" class="bg-[#303030] font-bold font-dmsans text-white px-6 py-3 rounded-[30px] text-[15px]">Login</a>
+        <a href="{{ route('login') }}" class="bg-[#303030] font-bold font-dmsans text-white px-6 py-3 rounded-[30px] text-[15px]">Login</a>
     </div>
 </nav>
