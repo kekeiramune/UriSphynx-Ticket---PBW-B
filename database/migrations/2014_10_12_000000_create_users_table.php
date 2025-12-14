4322f8b71a87b8bd665ac8a315087c7033b310a4
kekeiramune <kcinagas123@gmail.com>
2025-12-14 21:49:04 +0700

    password visibility update, blm fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            // Menambahkan kolom 'role' dengan default 'user'
+            $table->string('role')->default('user');
             $table->timestamps();
         });
     }
